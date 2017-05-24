1824ed5befe1bf5583a010cc51bddadfb8ff5ed4
Giancarlo Leandro <giancarlosleandro1@gmail.com>
2017-05-24 17:49:15 -0500

    Terminado sin cierre de tags

--- WindowsFormsApp4/WindowsFormsApp4/Form1.cs
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace WindowsFormsApp4
 {
@@ -15,6 +16,30 @@ namespace WindowsFormsApp4
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XmlTextReader xmlReader = new XmlTextReader("E:\\TEMPORALES\\XML\\archivo.xml");
+            String ultimaEtiqueta = "";
+            while (xmlReader.Read())
+            {
+                if (xmlReader.NodeType ==XmlNodeType.Element)
+                {
+                    richTextBox1.Text += (new String(' ',xmlReader.Depth*3) + "<" + xmlReader.Name + ">");
+                    ultimaEtiqueta = xmlReader.Name;
+                    continue;
+                }
+                if (xmlReader.NodeType == XmlNodeType.Text)
+                {
+                    richTextBox1.Text += xmlReader.ReadContentAsString() + "</" + ultimaEtiqueta + ">";
+                }
+                else
+                {
+                    richTextBox1.Text += "\r";
+                }
+            }
+
         }
     }
 }
